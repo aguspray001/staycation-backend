85769c68704f1216588271fc32c0fb8ea2de62aa
Agus Prayudi <aguspray@gmail.com>
2021-11-08 20:17:09 +0700

    update

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const adminController = require("../controllers/adminController");
+const { upload, uploadMultiple } = require("../middlewares/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 // endpoint category
@@ -7,9 +8,17 @@ router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
 router.put("/category", adminController.updateCategory);
 router.delete("/category/:id", adminController.deleteCategory);
-
+// endpoint bank
 router.get("/bank", adminController.viewBank);
+router.post("/bank", upload, adminController.addBank);
+router.put("/bank", upload, adminController.editBank);
+router.delete("/bank/:id", upload, adminController.deleteBank);
+// endpoint item
 router.get("/item", adminController.viewItem);
+router.post("/item", uploadMultiple, adminController.addItem);
+router.get("/item/show-image/:id", adminController.showImageItem);
+router.get("/item/:id", adminController.showEditItem);
+
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
